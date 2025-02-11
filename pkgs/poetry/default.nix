add0c0f286bd58d86cba72f910a3054e83e0a3ad
manuelbb-upb <manuelbb@mail.uni-paderborn.de>
2025-02-11 16:25:23 +0100

    add mlab path

--- pkgs/poetry/default.nix
+++ pkgs/poetry/default.nix
@@ -2,10 +2,29 @@
   lib,
   poetry,
   python-ld,
-  replaceDependency
+  matlab-engine,
+  replaceDependency,
 }:
-replaceDependency {
-  drv = poetry;
-  oldDependency = poetry.python;
-  newDependency = python-ld;
-}
+let
+  old-python = poetry.python;
+
+  ppoetry = replaceDependency {
+    drv = poetry;
+    oldDependency = old-python;
+    newDependency = python-ld;
+  };
+
+  mlab-engine = replaceDependency {
+    drv = (matlab-engine poetry.python.pkgs);
+    oldDependency = old-python;
+    newDependency = python-ld;
+  };
+
+  mlab-eng-path = "${mlab-engine}/${python-ld.sitePackages}";
+
+  pppoetry = ppoetry // {
+    inherit mlab-eng-path;
+  };
+in
+pppoetry
+#if export-matlab-engine-var then pppoetry else ppoetry
