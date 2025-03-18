1528a3ab4947adaff3dafad66d5f0914384cab74
manuelbb-upb <manuelbb@mail.uni-paderborn.de>
2025-03-18 19:48:11 +0100

    julia: repair indent in wrapper script

--- pkgs/julia/julia-prefixed.nix
+++ pkgs/julia/julia-prefixed.nix
@@ -1,56 +1,56 @@
 {
-  callOurPackage,
-  lib,
-  runCommand,
-  csh,
-  NIX_LD,
-  matlab,
-  version ? "1.11.1",
-  sha-for-version ? "",
-  add-opengl-libs ? true,
-  enable-matlab ? false,
-  pre_LD_LIBRARY_PATH ? "",
-  post_LD_LIBRARY_PATH ? ""
+	callOurPackage,
+	lib,
+	runCommand,
+	csh,
+	NIX_LD,
+	matlab,
+	version ? "1.11.1",
+	sha-for-version ? "",
+	add-opengl-libs ? true,
+	enable-matlab ? false,
+	pre_LD_LIBRARY_PATH ? "",
+	post_LD_LIBRARY_PATH ? ""
 }:
 
 let
-  julia-bin = callOurPackage ./julia-make-bin.nix { inherit version sha-for-version; };
+	julia-bin = callOurPackage ./julia-make-bin.nix { inherit version sha-for-version; };
 
-  # If building matlab was actual work, we would have to think about lazyDerivation here:
-  matlab_LD_LIBRARY_PATH = lib.optionalString enable-matlab matlab.LD_LIBRARY_PATH;
-  matlab-root = if enable-matlab then "\${${matlab.dir-env-var}}" else "";
-  # If it weren't for our peculiar needs, we should just set this to the
-  # empty string.
-  # Our wrapper **replaces** the system-wide `NIX_LD_LIBRARY_PATH` with this
-  # string.
-  # If there is weird stuff in this path, then Julia will do weird things.
-  # It usually is sensitive to fickling with `LD_LIBRARY_PATH`.
-  # For `GLMakie` however, we want `/run/opengl-driver/lib` to be visible.
-  # That is easy enough.
-  # But we also need the `matlab_LD_LIBRARY_PATH` for `MATLAB.jl` to work.
-  # Here it gets complicated.
-  # We explicitly add the Julia libs to the library path **before** the 
-  # system libs, hoping that they get sourced first.
-  julia_LD_LIBRARY_PATH = pre_LD_LIBRARY_PATH + 
-    "${julia-bin}/lib:${julia-bin}/lib/julia:" +
-    lib.optionalString add-opengl-libs "/run/opengl-driver/lib:" +
-    matlab_LD_LIBRARY_PATH + 
-    post_LD_LIBRARY_PATH;
-  
-  csh-path = if enable-matlab then "${csh}/bin" else "";
+	# If building matlab was actual work, we would have to think about lazyDerivation here:
+	matlab_LD_LIBRARY_PATH = lib.optionalString enable-matlab matlab.LD_LIBRARY_PATH;
+	matlab-root = if enable-matlab then "\${${matlab.dir-env-var}}" else "";
+	# If it weren't for our peculiar needs, we should just set this to the
+	# empty string.
+	# Our wrapper **replaces** the system-wide `NIX_LD_LIBRARY_PATH` with this
+	# string.
+	# If there is weird stuff in this path, then Julia will do weird things.
+	# It usually is sensitive to fickling with `LD_LIBRARY_PATH`.
+	# For `GLMakie` however, we want `/run/opengl-driver/lib` to be visible.
+	# That is easy enough.
+	# But we also need the `matlab_LD_LIBRARY_PATH` for `MATLAB.jl` to work.
+	# Here it gets complicated.
+	# We explicitly add the Julia libs to the library path **before** the 
+	# system libs, hoping that they get sourced first.
+	julia_LD_LIBRARY_PATH = pre_LD_LIBRARY_PATH + 
+		"${julia-bin}/lib:${julia-bin}/lib/julia:" +
+		lib.optionalString add-opengl-libs "/run/opengl-driver/lib:" +
+		matlab_LD_LIBRARY_PATH + 
+		post_LD_LIBRARY_PATH;
+	
+	csh-path = if enable-matlab then "${csh}/bin" else "";
 in
 runCommand "${julia-bin.pname}-ld-${julia-bin.version}" {
-  inherit (julia-bin) pname version;
+	inherit (julia-bin) pname version;
 } ''
-  mkdir -p $out/bin
-  cat << "EOF" > $out/bin/julia
-    #! /usr/bin/env bash
-    export PATH="${csh-path}:''${PATH}"
-    NIX_LD="${NIX_LD}"
-    export NIX_LD
-    export NIX_LD_LIBRARY_PATH="${julia_LD_LIBRARY_PATH}"
-    export MATLAB_ROOT="${matlab-root}"
-    exec -a "$0" "${julia-bin}/bin/julia" "$@"
-  EOF
-  chmod a+x $out/bin/julia
+	mkdir -p $out/bin
+	cat <<-"EOF" > $out/bin/julia
+		#! /usr/bin/env bash
+		export PATH="${csh-path}:''${PATH}"
+		NIX_LD="${NIX_LD}"
+		export NIX_LD
+		export NIX_LD_LIBRARY_PATH="${julia_LD_LIBRARY_PATH}"
+		export MATLAB_ROOT="${matlab-root}"
+		exec -a "$0" "${julia-bin}/bin/julia" "$@"
+	EOF
+	chmod a+x $out/bin/julia
 ''
--- pkgs/python-matlab-engine/default.nix
+++ pkgs/python-matlab-engine/default.nix
@@ -1,83 +1,83 @@
 {
-  formats,
-  lib,
-  matlab,
-  pname ? "matlab",
-  version ? "0.0.1",
+	formats,
+	lib,
+	matlab,
+	pname ? "matlab",
+	version ? "0.0.1",
 }:
 py-pkgs:
 let
-  # The file `path_hook.py` defines a hook for `sys.meta_path` to
-  # generate the architecture file if `matlab` or `matlab.engine` is
-  # imported in Python.
-  # We provide it as a source file for our shim package.
-  # This can be done as follows:
-  fs = lib.fileset;
-  path_hook_src = fs.toSource {
-    root = ./.;
-    fileset = ./path_hook.py;
-  };
+	# The file `path_hook.py` defines a hook for `sys.meta_path` to
+	# generate the architecture file if `matlab` or `matlab.engine` is
+	# imported in Python.
+	# We provide it as a source file for our shim package.
+	# This can be done as follows:
+	fs = lib.fileset;
+	path_hook_src = fs.toSource {
+		root = ./.;
+		fileset = ./path_hook.py;
+	};
 
-  # Most of the code below is copied from 
-  # `pkgs/development/interpreters/python/editable.nix`.
-  # Our shim package also only is a `.pth` file pointing to the real
-  # engine code.
-  # Usually, build variable `${root}` is an escaped environment variable 
-  # (like MATLAB_INSTALL_DIR).
-  # In the build environment, this variable is not accessible.
-  # But the `_matlab.pth` file is sourced after build.
-  # Python can expand the environment variable and then import `matlab`.
-  # To keep this dynamic behavior, we do not make `_matlab.pth` part of the
-  # `src` files for the build, as we would have to do manipulations on the 
-  # file anyways in the `unpackPhase`.
-  pyproject = {
-    project = {
-      inherit version;
-      name = pname;
-      requires-python = ">=3.8";
-    };
-    # Allow empty package
-    tool.hatch.build.targets.wheel.bypass-selection = true;
+	# Most of the code below is copied from 
+	# `pkgs/development/interpreters/python/editable.nix`.
+	# Our shim package also only is a `.pth` file pointing to the real
+	# engine code.
+	# Usually, build variable `${root}` is an escaped environment variable 
+	# (like MATLAB_INSTALL_DIR).
+	# In the build environment, this variable is not accessible.
+	# But the `_matlab.pth` file is sourced after build.
+	# Python can expand the environment variable and then import `matlab`.
+	# To keep this dynamic behavior, we do not make `_matlab.pth` part of the
+	# `src` files for the build, as we would have to do manipulations on the 
+	# file anyways in the `unpackPhase`.
+	pyproject = {
+		project = {
+			inherit version;
+			name = pname;
+			requires-python = ">=3.8";
+		};
+		# Allow empty package
+		tool.hatch.build.targets.wheel.bypass-selection = true;
 
-    # Include our editable pointer file in build
-    tool.hatch.build.targets.wheel.force-include."_${pname}.pth" = "_${pname}.pth";
+		# Include our editable pointer file in build
+		tool.hatch.build.targets.wheel.force-include."_${pname}.pth" = "_${pname}.pth";
 
-    # Also include the hook
-    tool.hatch.build.targets.wheel.force-include."path_hook.py" = "path_hook.py";
+		# Also include the hook
+		tool.hatch.build.targets.wheel.force-include."path_hook.py" = "path_hook.py";
 
-    # Also copy pyproject.toml for poetry trick 
-    tool.hatch.build.targets.wheel.force-include."pyproject.toml" = "pyproject.toml";
+		# Also copy pyproject.toml for poetry trick 
+		tool.hatch.build.targets.wheel.force-include."pyproject.toml" = "pyproject.toml";
 
-    # Build editable package using hatchling
-    build-system = {
-      requires = [ "hatchling" ];
-      build-backend = "hatchling.build";
-    };
-  };
+		# Build editable package using hatchling
+		build-system = {
+			requires = [ "hatchling" ];
+			build-backend = "hatchling.build";
+		};
+	};
 
-  pyproject-toml = (formats.toml {}).generate "pyproject.toml" pyproject;
+	pyproject-toml = (formats.toml {}).generate "pyproject.toml" pyproject;
 in
-  py-pkgs.buildPythonPackage {
-    inherit pname version;
-    src = path_hook_src;
-    pyproject = true;
-    build-system = [
-      py-pkgs.hatchling
-    ];
-    preferLocalBuild = true;
-    
-    unpackPhase = ''
-      # Make hook file available
-      cp $src/path_hook.py path_hook.py
+	py-pkgs.buildPythonPackage {
+		inherit pname version;
+		src = path_hook_src;
+		pyproject = true;
+		build-system = [
+			py-pkgs.hatchling
+		];
+		preferLocalBuild = true;
+		
+		unpackPhase = ''
+			# Make hook file available
+			cp $src/path_hook.py path_hook.py
 
-      # Copy the generated toml file
-      cp ${pyproject-toml} pyproject.toml
+			# Copy the generated toml file
+			cp ${pyproject-toml} pyproject.toml
 
-      # Write `_matlab.pth`.
-      # Lines starting with `import` are executed.
-      cat <<- "EOF" > "_${pname}.pth"
-      import os, sys; sys.path.insert(0, os.path.join(os.path.expandvars("''$${matlab.dir-env-var}"), "extern", "engines", "python", "dist"));
-      import path_hook  # executes code to prepend to sys.meta_path
-      EOF
-    '';
-  }
+			# Write `_matlab.pth`.
+			# Lines starting with `import` are executed.
+			cat <<- "EOF" > "_${pname}.pth"
+			import os, sys; sys.path.insert(0, os.path.join(os.path.expandvars("''$${matlab.dir-env-var}"), "extern", "engines", "python", "dist"));
+			import path_hook	# executes code to prepend to sys.meta_path
+			EOF
+		'';
+	}
